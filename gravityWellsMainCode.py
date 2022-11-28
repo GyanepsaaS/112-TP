d3dd16f1d643317d549000b9e697aff97c5ffac7
GyanepsaaS <64652949+GyanepsaaS@users.noreply.github.com>
2022-11-27 23:19:56 -0500

    Basic visual implement - 2 body system

--- gravityWellsMainCode.py
+++ gravityWellsMainCode.py
@@ -1,24 +1,65 @@
 from cmu_112_graphics import *
+import random
+
+def appStarted(app):
+    #temp code
+    earthMass = 5.97 * 10**24
+    earthRadius = 6400000
+    orbitalRadius = 10**7
+    universe = SpaceTime()
+    objMass = 1
+    orbitalVelocity = (universe.G * earthMass/orbitalRadius)**0.5
+
+    app.positionScale = 10**(-5)   # scaling position (which uses realistic scaling) such that it fits on canvas
+
+    # converting starting positions on canvas to 'real' scale for initial position arguments
+    scaledcx = (app.width//2)/app.positionScale
+    scaledcy = (app.height//2)/app.positionScale
+
+    app.player = Player(objMass, [scaledcx, scaledcy-orbitalRadius], [orbitalVelocity, 0], [0, 0])
+    app.planets = []
+    app.gravityWell = GravityWell(earthMass, [scaledcx, scaledcy])
+    app.timerDelay = 100
+    app.universe = SpaceTime()
 
 # Main Classes
 
+# all objects on board (planets, player, gravity wells) are instances of class Satellite
 class Satellite(object):
     def __init__(self, m, pos, v, a):
         self.pos = pos  # position as a vector
         self.v = v   # velocity as a vector
         self.a = a  # acceleration as a vector
         self.aPrev = a
-        self.m = m  # mass - replace later with density and radius
+        self.m = m  # mass 
+        self.r = random.randint(5, 25)
+        self.colour = 'cyan'
+
+    def draw(self, app, canvas):
+        scaledPos = [self.pos[0]*app.positionScale, self.pos[1]*app.positionScale]
+        print(self.colour, scaledPos)
+        x0, y0 = scaledPos[0]-self.r, scaledPos[1]-self.r
+        x1, y1 = scaledPos[0]+self.r, scaledPos[1]+self.r
+        canvas.create_oval(x0, y0, x1, y1, fill=self.colour)
 
 class GravityWell(Satellite):
     def __init__(self, m, pos, v = [0, 0], a = [0, 0]):
         super().__init__(m, pos, v, a)
+        self.r = random.randint(25, 100)
+        self.colour = 'black'
 
+class Player(Satellite):
+    def __init__(self, m, pos, v, a):
+        super().__init__(m, pos, v, a)
+        self.r = 10
+        self.colour = 'orange'
+
+# class SpaceTime contains implementation of gravity
 class SpaceTime(object):
     def __init__(self):
         self.G = 6.6743 * 10**(-11)  # Gravitational constant
         self.time = 0
-        self.dt = 1  # timestep
+        self.dt = 100  # timestep
 
     def updatePos(self, on):
         # updating position for vector components individually
@@ -54,49 +95,58 @@ class SpaceTime(object):
         self.updateVelocity(on)
         self.updateTime()
 
+def timerFired(app):
+    app.universe.updateAll(app.player, app.gravityWell)
 
-# UI/UX
-def appStarted(app):
-    pass
+def redrawAll(app, canvas):
+    app.player.draw(app, canvas)
+    for planet in app.planets:
+        planet.draw(app, canvas)
+    app.gravityWell.draw(app, canvas)
+
+def playGravityWells():
+    runApp(width = 600, height = 600)
+
+playGravityWells()
 
 
 # Tests
 
-def gravNumericTest():
-    print("gravTest")
-    earthMass = 5.97 * 10**24
-    earthRadius = 6400000
-    universe = SpaceTime()
-    objMass = 1
-    object = Satellite(objMass, [0, 100], [0, 0], [0, 0])
-    earth = GravityWell(earthMass, [0, -earthRadius], [0, 0], [0, 0])
+# def gravNumericTest():
+#     print("gravTest")
+#     earthMass = 5.97 * 10**24
+#     earthRadius = 6400000
+#     universe = SpaceTime()
+#     objMass = 1
+#     object = Satellite(objMass, [0, 100], [0, 0], [0, 0])
+#     earth = GravityWell(earthMass, [0, -earthRadius], [0, 0], [0, 0])
 
-    while object.pos[1] >= 0 and object.pos[0] >= 0:
-        universe.updateAll(object, earth)
-        print(f"pos = {object.pos}")
-        print(f"v = {object.v}")
-        print()
+#     while object.pos[1] >= 0 and object.pos[0] >= 0:
+#         universe.updateAll(object, earth)
+#         print(f"pos = {object.pos}")
+#         print(f"v = {object.v}")
+#         print()
 
-def orbitNumericTest():
-    print("orbitTest")
-    earthMass = 5.97 * 10**24
-    earthRadius = 6400000
-    orbitalRadius = 10**7
-    universe2 = SpaceTime()
-    objMass = 1
-    orbitalVelocity = (universe2.G * earthMass/orbitalRadius)**0.5
-    object = Satellite(objMass, [0, orbitalRadius], [orbitalVelocity, 0], [0, 0])
-    earth = GravityWell(earthMass, [0, 0], [0, 0], [0, 0])
+# def orbitNumericTest():
+#     print("orbitTest")
+#     earthMass = 5.97 * 10**24
+#     earthRadius = 6400000
+#     orbitalRadius = 10**7
+#     universe2 = SpaceTime()
+#     objMass = 1
+#     orbitalVelocity = (universe2.G * earthMass/orbitalRadius)**0.5
+#     object = Satellite(objMass, [0, orbitalRadius], [orbitalVelocity, 0], [0, 0])
+#     earth = GravityWell(earthMass, [0, 0], [0, 0], [0, 0])
 
-    while universe2.time < 100:
-        universe2.updateAll(object, earth)
-        print(f"pos = {object.pos}")
-        print(f"v = {object.v}")
-        orbitalR = (object.pos[0]**2 + object.pos[1]**2)**0.5
-        print(f"orbitalR = {orbitalR}")
-        orbitalV = (object.v[0]**2 + object.v[1]**2)**0.5
-        print(f"orbitalV = {orbitalV}")
-        print()
+#     while universe2.time < 100:
+#         universe2.updateAll(object, earth)
+#         print(f"pos = {object.pos}")
+#         print(f"v = {object.v}")
+#         orbitalR = (object.pos[0]**2 + object.pos[1]**2)**0.5
+#         print(f"orbitalR = {orbitalR}")
+#         orbitalV = (object.v[0]**2 + object.v[1]**2)**0.5
+#         print(f"orbitalV = {orbitalV}")
+#         print()
 
 # gravNumericTest()
 # orbitNumericTest()
